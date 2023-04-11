5561635c91f80ebd1bb852ad0af94abd4925faad
Jimmy Cha <jbcha@usc.edu>
2023-04-11 03:33:56 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(int row, int col, vector<size_t>& maxShifts, DailySchedule& sched, AvailabilityMatrix& avail, int dailyNeed);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +38,56 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    AvailabilityMatrix temp = avail;
+    //fill sched with placeholders
+    //and set maximum shifts for each worker
 
+    for (int r = 0; r < avail.size(); r++){
+        vector<Worker_T> temp(dailyNeed, -1);
+        sched.push_back(temp);
+    }
 
+    //vector with max shifts, where each index connects to each worker #
+    vector<size_t> maxShiftVector;
+    for (int i = 0; i < avail[0].size(); i++) {
+        maxShiftVector.push_back(maxShifts);
+    }
 
+    return scheduleHelper(0, 0, maxShiftVector, sched, temp, dailyNeed);
+}
 
+bool scheduleHelper(int row, int col, vector<size_t>& maxShifts, DailySchedule& sched, AvailabilityMatrix& avail, int dailyNeed) {
+    //if reach the end of a "day", go to next day
+    if (row == avail.size()) {
+        return true;
+    }
+    //if size of the row = the max shifts, go to the next row
+    else if (col == dailyNeed) {
+        return scheduleHelper(row+1, 0, maxShifts, sched, avail, dailyNeed);
+    }
+    //if schedule has not been started yet
+    else {
+        for (int i = 0; i < avail[0].size(); i++) {
+            //check if worker I is available for the day && if the worker has available shifts
+            if (avail[row][i] == 1 && maxShifts[i] > 0) {
+                // assign worker i to that day, 
+                sched[row][col] = i;
+                // remove availability for that day, (change avail value to 0 for that day)
+                avail[row][i] = 0;
+                // decrease total available shifts of WOrker I by 1,
+                maxShifts[i]-=1;
+                // recurse to the next available spot
+                if (scheduleHelper(row, col+1, maxShifts, sched, avail, dailyNeed)) {
+                    //reached end, so return true
+                    return true;
+                }
+                sched[row][col] = -1;
+                avail[row][i] = 1;
+                maxShifts[i] += 1;
+                //and set max shifts to +1
+            }
+        }
+    }
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,106 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(int emptySpots, int currentLocation, string floating, string& word, set<string>& dict, set<string>& validWords, set<char> floatChars);
 
 // Definition of primary wordle function
+/*
+ * test with: 
+ *./wordle-driver -i-- dn
+ * example input: 
+ * wordle(-i-- dn)
+ */
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<std::string> validWords;
+    string fltChars = floating;
+    set<char> floatChars;
+    for (char i : fltChars) {
+        floatChars.insert(i);
+    }
+    string word = in;
+    set<string> dictionary = dict;
+    int emptySpots = 0;
+    for (unsigned int i = 0; i < in.length(); i++) {
+        //if empty spot, ....
+        if (in[i] == '-') {
+            emptySpots++;
+        }
+    }
+    wordleHelper(emptySpots, 0, fltChars, word, dictionary, validWords, floatChars);
+    return validWords;
 }
 
-// Define any helper functions here
+void wordleHelper(
+    int emptySpots, 
+    int currentLocation, 
+    string floating, 
+    string& word, 
+    set<string>& dict,
+    set<string>& validWords,
+    set<char> floatChars) 
+{
+
+    //use a map to check how many times we have used each float
+    //check the size of the remaining floating to the number of empty emptySpots
+    //place the alphabet one by one and check the remaining spots 
+    //if the size of the floating is less than or equal to the number of empty spots, place them in the spots
+
+    //if we have itereated through the whole word
+    if (currentLocation >= word.size()) {
+        if (dict.find(word) != dict.end()) {
+            validWords.insert(word);
+            return;
+        }
+        else {
+            return;
+        }
+    }
+    //if the current location is an empty spot
+    if (word[currentLocation] != '-') {
+        //cout <<"working";
+        wordleHelper(emptySpots, currentLocation + 1, floating, word, dict, validWords, floatChars);
+    }
+    else {
+        //if there are remaining floating characters
+        if (floating.size() == emptySpots) {
+            //set<char> temp = floatChars;
+            string temp = floating;
+            //set<char>::iterator it = temp.begin();
+            for (auto i : temp) {
+                word[currentLocation] = i;
+                //floatChars.erase(*it);
+                floating.erase(floating.find(i), 1);
+                wordleHelper(emptySpots - 1, currentLocation + 1, floating, word, dict, validWords, floatChars);
+                //floatChars.insert(*it);
+                floating+=i;
+                word[currentLocation] = '-';
+            }
+        }
+        //if there are no floating characters left
+        else {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (char letter : alphabet) {
+                //check if the letter in the alphabet has been used already in the float
+                //word[currentLocation - 1] = letter;
+                if (floating.find(letter) != std::string::npos) {
+                    word[currentLocation] = letter;
+                    floating.erase(floating.find(letter), 1);
+                    wordleHelper(emptySpots - 1, currentLocation + 1, floating, word, dict, validWords, floatChars);
+                    word[currentLocation] = '-';
+                    floating+=letter;
+                }
+                else {
+                    word[currentLocation] = letter;
+                    wordleHelper(emptySpots - 1, currentLocation + 1, floating, word, dict, validWords, floatChars);
+                    word[currentLocation] = '-';
+                }
+            }
+        }
+    }
+    return;
+}
